2614b276f061629b0b082e386aed6f11bb0ede89
vasilyevruslan <75842280+vasilyevruslan@users.noreply.github.com>
2020-12-22 12:46:29 +0200

    Add files via upload
    я надеюсь я правильно загрузил??

---
+++ русланхы/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace русланхы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S, y, t;
+            Console.Write("Введите y");
+            y = double.Parse(Console.ReadLine());
+            Console.Write("Введите t:");
+            t = double.Parse(Console.ReadLine());
+            S = 4.351 * y * y * y + 2 * t *Math.Log(t) / Math.Sqrt(Math.Cos(2 * y + 4.351));
+            
+        }
+    } 
+}
